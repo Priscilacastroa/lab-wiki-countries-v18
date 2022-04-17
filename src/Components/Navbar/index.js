13b1508f9827974ea2d9011e40d27a112461cb66
Priscila Castro <priscilacastroa@gmail.com>
2022-04-17 17:36:48 -0300

    inicio

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,17 @@
+
+export function Navbar() {
+    return (
+        <>
+        <nav class="navbar navbar-dark bg-primary">
+            <div class ="container-fluid">
+                <span class="navbar-brand mb-0 h1" >LAB-WikiCountries </span>
+            </div>
+        </nav>
+
+        </>
+
+    )
+
+
+}
+
